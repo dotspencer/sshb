6f72b11db29f333094a752895ef69b74e06db2b2
Spencer Smith <dontaskspencer@gmail.com>
2017-04-14 01:08:56 -0600

    Added comments. Prints keys with padding

--- functions.js
+++ functions.js
@@ -1,13 +1,49 @@
+var fs = require('fs');
+var child = require('child_process');
+
+/*-=-=-=-=-
+  Shows the list of bookmarks
+-=-=-=-=-*/
 function showList(list){
+  var longest = getLongestKeyLength(list);
+
   console.log();
   for (var key in list) {
     if (list.hasOwnProperty(key)) {
-      console.log(`${key} -> "${list[key]}"`);
+      var pad = createSpace(longest - key.length);
+      console.log(`${key + pad} -> "${list[key]}"`);
     }
   }
   console.log();
 }
 
+/*-=-=-=-=-
+  Creates a whitespace-filled string of the specified length
+-=-=-=-=-*/
+function createSpace(length){
+  var space = "";
+  for (var i = 0; i < length; i++) {
+    space += " ";
+  }
+  return space;
+}
+
+/*-=-=-=-=-
+  Gets the length of the longest alias string
+-=-=-=-=-*/
+function getLongestKeyLength(list){
+  var longest = 0;
+  for (var key in list) {
+    if(list.hasOwnProperty(key) && key.length > longest){
+      longest = key.length;
+    }
+  }
+  return longest;
+}
+
+/*-=-=-=-=-
+  Adds a new bookmark to the list
+-=-=-=-=-*/
 function addNew(args, list){
   var key = args[1];
   var bookmark = args[2];
@@ -18,25 +54,48 @@ function addNew(args, list){
   });
 }
 
-function copyCommand(args){
-  var key = args[0];
-  if(list[key] != null){
-    child.exec(`echo "${list[key]}"  | pbcopy`);
-    console.log("--- Paste and you're off! ---");
-  } else {
-    console.log("Bookmark not found.");
-  }
+/*-=-=-=-=-
+  Removes a bookmark from the list
+-=-=-=-=-*/
+function remove(args, list){
+  var key = args[1];
+  var existed = list.hasOwnProperty(key);
+  delete list[key];
+  fs.writeFile(`${__dirname}/list.json`, JSON.stringify(list), function(err){
+    if(err) throw err;
+    var msg = existed ? "Bookmark removed." : "Bookmark not found.";
+    console.log(msg);
+  });
 }
 
+/*-=-=-=-=-
+  Copies the command to be pasted
+-=-=-=-=-*/
+function copyCommand(args, list){
+  var key = args[0];
+  var exists = list.hasOwnProperty(key);
+  var msg = "Bookmark not found.";
+  if(exists){
+    child.exec(`echo "${list[key]}"  | pbcopy`);
+    msg = "--- Paste and you're off! ---";
+  }
+  console.log(msg);
+}
+
+/*-=-=-=-=-
+  Shows error messages
+-=-=-=-=-*/
 function showError(){
   console.log("\nTry running either of the following:");
   console.log("• sshb list");
-  console.log("• sshb add '<shortname>' 'ssh ___@coolsite.com'\n");
+  console.log("• sshb add '<alias>' 'ssh ___@coolsite.com'");
+  console.log("• sshb remove '<alias>'\n");
 }
 
 module.exports = {
   showList: showList,
   addNew: addNew,
+  remove: remove,
   copyCommand: copyCommand,
   showError: showError
 }
--- sshb.js
+++ sshb.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-var fs = require('fs');
-var child = require('child_process');
-
 var f = require(`${__dirname}/functions.js`);
 var list = require(`${__dirname}/list.json`);
 var args = process.argv.splice(2);
@@ -16,9 +13,14 @@ if(args.length == 3 && args[0] == 'add' && args[1].length > 0 && args[2].length 
   return f.addNew(args, list);
 }
 
+// Remove bookmark
+if(args.length == 2 && args[0] == 'remove' && args[1].length > 0){
+  return f.remove(args, list);
+}
+
 // Copies command to paste and run
 if(args.length == 1){
-  return f.copyCommand(args);
+  return f.copyCommand(args, list);
 }
 
 // Invalid command
